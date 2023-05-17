28d525e33f83a50cd531c28f7e84e28aca451b85
tariqabusada <tariqesa101@gmail.com>
2023-05-17 17:13:41 +0300

    build: Acion Button for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import Navbar from "@/components/navbar"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { SelectedPage } from "@/shared/types";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(()=> {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => removeEventListener("scroll", handleScroll)
+  }, []);
 
   return (
-    <div className="app bg-gray-20 bg-gray-50">
+    <div className="app bg-gray-20">
       <Navbar 
+        isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />

48f780054e25f2ed514bb492e6e4c540b55673cc
tariqabusada <tariqesa101@gmail.com>
2023-05-17 15:15:47 +0300

    setup and configrations

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Navbar from "@/components/navbar"
+import { useState } from "react"
+import { SelectedPage } from "@/shared/types";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  return (
+    <div className="app bg-gray-20 bg-gray-50">
+      <Navbar 
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/shared/types.ts
@@ -0,0 +1,6 @@
+export enum SelectedPage {
+    Home = "home",
+    Benefits = "benefits",
+    OurServices ="ourservices",
+    ContactUs ="contactus"
+  }

9037d1f8efcc2f7f8a9ee1d9bd3474f39f84941f
tariqabusada <tariqesa101@gmail.com>
2023-05-19 17:02:06 +0300

    fix: the background color of navbar where it is on the top of page

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -17,7 +17,7 @@ const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}: Props) => {
   const flexBetween = "flex items-center justify-between";
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
-  const navbarBackground = isTopOfPage ? "bg-primary-100 drop-shadow" : "";
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
   return <nav>
   <div 
